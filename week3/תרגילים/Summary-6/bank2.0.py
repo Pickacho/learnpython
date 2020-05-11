fe839a6033283c58634b67c4a465ae94bb25d8d0
Ran El <grandfathermagic@gmail.com>
2020-05-11 21:57:13 +0300

    בנק 2 שבוע 2 התוכנה כעת יודעת לקבל שם משתמש - לבדוק האם הוא שווה לאדמין והסיסמא שלו שווה לשגוי במידה ולא - חוזר לבדוק אותנו ועובד בלולאה
    Signed-off-by: Pickacho <40912416+Pickacho@users.noreply.github.com>

--- week3/תרגילים/Summary-6/bank2.0.py
+++ week3/תרגילים/Summary-6/bank2.0.py
@@ -1,5 +1,7 @@
 ###################################
 # This is the Givov Function: will get password string and return out_put_hash
+
+
 def my_hash(string):
     i = 0
     temp_hash = 1
@@ -33,8 +35,6 @@ def check_user_and_password(username, password):
             print("I have got your input\n \n \n")
             if username == "wrong" or "admin":
                 print("Great ! this user is available")
-                print("Please enter the password")
-                password = input("Enter your Password please")
                 if (username == "wrong" and password == "ads sports") or (
                         username == "admin" and password == "is traitor"):
                     print("you are now logged in")
@@ -43,8 +43,11 @@ def check_user_and_password(username, password):
                     print("one  or both of the details are wrong")
                     try_again = 0
                     username = input("Enter a new user name")
+                    password = input("Enter your Password please")
 
 
 user_input = input("what is the user name you are trying to log with? ")
 password_input = input("what is the password of the user?")
 check_user_and_password(user_input, password_input)
+
+print(my_hash("my name is Ran"))
