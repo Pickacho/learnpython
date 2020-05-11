f34da902df9ab5a55383f034488c0bd444a8dcfa
Pickacho <40912416+Pickacho@users.noreply.github.com>
2020-05-11 22:21:04 +0300

    Add files via upload

--- week2/Bank2.py
+++ week2/Bank2.py
@@ -1,32 +1,36 @@
-##תוכנה זו תציג למשתמש ברכה ולאחר מכן תבקש ממנו משתמש וסיסמא ותגיב בהתאם למה שיכניס
-print("Dear user, wellcome to our system")
+#  תוכנה זו תציג למשתמש ברכה ולאחר מכן תבקש ממנו משתמש וסיסמא ותגיב בהתאם למה שיכניס
+# print("Dear user, welcome to our system")
+# print('Before you will be able to login you will need to first...')
+# username = input("Enter your user name ")
+
+# if username == "wrong" or "admin":
+#     PaSSword = input("Enter your Password please")
+#     if (username == "wrong" and PaSSword == "ads sports") or (username == "admin" and PaSSword == "is traitor"):
+#         print("you are now logged in")
+#     else:
+#         print("one  or both of the details are wrong")
+
+# תוכנה זו תציג למשתמש ברכה ולאחר מכן תבקש ממנו משתמש וסיסמא ותגיב בהתאם למה שיכניס
+print("Dear user, welcome to our system")
 print("Before you will be able to login you will need to first...")
-username = input("Enter your user name ")
 
-login = False
-
-if username == "wrong" or "admin":
-    PaSSword = input("Enter your Password please")
-    if (username == "wrong" and PaSSword == "ads sports") or  (username == "admin" and PaSSword == "is trator"):
-        print("you are now logged in")
+try_again = 0
+while try_again == 0:
+    username = input("Enter your user name ")
+    if username != "wrong" and username != "admin":
+        print("No Such user")
+        try_again = 0
+        print("you will need to try something else")
+        continue
     else:
-        print("one  or both of the details are wrong")
-
-
-##תוכנה זו תציג למשתמש ברכה ולאחר מכן תבקש ממנו משתמש וסיסמא ותגיב בהתאם למה שיכניס
-print("Dear user, wellcome to our system")
-print("Before you will be able to login you will need to first...")
-username = input("Enter your user name ")
-
-if username != "wrong" and username != "admin":
-    print("No Such user")
-else:
-    print("Ok")
-    if username == "wrong" or "admin":
-        print("Great ! this user is available")
-        print("Please enter the password")
-        PaSSword = input("Enter your Password please")
-        if (username == "wrong" and PaSSword == "ads sports") or (username == "admin" and PaSSword == "is trator"):
-            print("you are now logged in")
-        else:
-            print("one  or both of the details are wrong")
+        print("I have got your input\n \n \n")
+        if username == "wrong" or "admin":
+            print("Great ! this user is available")
+            print("Please enter the password")
+            PaSSword = input("Enter your Password please")
+            if (username == "wrong" and PaSSword == "ads sports") or (username == "admin" and PaSSword == "is trator"):
+                print("you are now logged in")
+                try_again = 1
+            else:
+                print("one  or both of the details are wrong")
+                try_again = 0
