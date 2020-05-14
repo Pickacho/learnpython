59a51a9350ff8d298e28ab970cfcfe650579f8c1
Ran El <grandfathermagic@gmail.com>
2020-05-14 14:10:23 +0300

    Line / Row clear to understand X or O now works for each turn

--- week3/תרגילים/Summary-6/Tick_tac_toe/coordinates.py
+++ week3/תרגילים/Summary-6/Tick_tac_toe/coordinates.py
@@ -1,5 +1,19 @@
 var = [['X', 'O', 'X'], ['X', 'X', '-'], ['O', '-', 'X']]
-
+####################################################################
+# column  0         -1-         2
+top_to_button0 = var[0][0] == var[1][0] == var[2][0]
+top_to_button1 = var[0][1] == var[1][1] == var[2][1]
+top_to_button2 = var[0][2] == var[1][2] == var[2][2]
+####################################################################
+# lines    0         -1-         2
+top_line = var[0][0] == var[0][1] == var[0][2]
+middle_line = var[1][0] == var[1][1] == var[1][2]
+bottom_line = var[2][0] == var[2][1] == var[2][2]
+####################################################################
+# diagonal
+diagonalL_to_R = var[0][0] == var[1][1] == var[2][2]
+diagonalR_to_L = var[0][2] == var[1][1] == var[0][0]
+####################################################################
 player1 = "O"
 player2 = "X"
 
@@ -33,18 +47,24 @@ while counter < 9:
     print(board[1])
     print(board[2])
     print(player % 2)
-    counter += 1
-    list_num = int(input("pick a list_num"))
-    place_holder = int(input("pick a place_holder"))
-    if player == 1:
-        marker = "0"
-        try:
-            board[list_num][place_holder] = marker
-        except:
-            pass
+    player = counter % 2
+    list_num = int(input("pick a line"))
+    place_holder = int(input("pick a row"))
+    spot_player_picked = board[list_num][place_holder]
+    if spot_player_picked != player1 and spot_player_picked != player2:
+        if player == 1:
+            counter += 1
+            marker = "0"
+            try:
+                board[list_num][place_holder] = marker
+            except:
+                pass
+        else:
+            counter += 1
+            marker = "X"
+            try:
+                board[list_num][place_holder] = marker
+            except:
+                pass
     else:
-        marker = "X"
-        try:
-            board[list_num][place_holder] = marker
-        except:
-            pass
+        print("You picked already used spot, pick different spot")
